d6cee0c0661844a0acb9ac389ab024dbbd09a761
Javier Arias <javierariashe@gmail.com>
2024-06-29 01:11:50 -0600

    add js file and dynamic features

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+const operators = "/*-+";
+
 // Basic Operations
 
 add = (a,b) => a+b;
@@ -5,37 +7,122 @@ subsract = (a,b) => a-b;
 multiply = (a,b) => a*b;
 divide = (a,b) => a/b;
 
-function read(string) {
-    let operation = string.split(" ");
-    if(operation.length != 3) {
-        console.log("Incorrect format. 'number' 'operator 'number'.");
-        return "ERROR";
-    }
-
-    number1 = Number(operation[0]);
-    operator = operation[1];
-    number2 = Number(operation[2]);
+function operate(number1, operator, number2) {
+    number1 = Number(number1);
+    number2 = Number(number2);
 
     switch(operator) {
         case "+":
-            console.log(add(number1, number2));
-            break;
+            return add(number1, number2);
         case "-":
-            console.log(subsract(number1, number2));
-            break;
+            return subsract(number1, number2);
         case "*":
-            console.log(multiply(number1, number2));
-            break;
+            return multiply(number1, number2);
         case "/":
-            console.log(divide(number1, number2));
-            break;
+            return divide(number1, number2);
         default:
             console.log("Operador invalido"); 
             return "ERROR";
-            break;
     }
 }
 
-read("4 + 3");
-read("10 * 2");
+function pressButton(button) {
+    let displayNumber = "";
 
+    if(!isNaN(parseInt(button))) {
+        displayNumber = pressNumber(button);
+    }
+    else if(button === '.') {
+        displayNumber = pressPoint(button);
+    }
+    else if(operators.includes(button)) {
+        displayNumber = pressOperator(button);
+        return;
+    }
+    else if(button === "=") {
+        displayNumber = pressEquals(button);
+    }
+    else {
+        displayNumber = pressDelete(button);
+    }
+
+    if(displayNumber % 1 != 0) displayNumber = parseFloat(displayNumber).toFixed(4);
+    if(displayNumber == 'Infinity' || displayNumber == '-Infinity') displayNumber = "ERROR";
+    screen.textContent = displayNumber;
+}
+
+function pressNumber(button) {
+    if(answer) {
+        previousNumber = currentNumber; 
+        currentNumber = "";
+        answer = false;
+    }
+    currentNumber += button;
+    return currentNumber;
+}
+
+function pressPoint(button) {
+    if(point) return;
+
+    if(answer) {
+        previousNumber = currentNumber; 
+        currentNumber = "";
+        answer = false;
+    }
+    currentNumber += button;
+    point = true;
+    return currentNumber;
+}
+
+function pressOperator(button) {
+    if(operator != "") {
+        currentNumber = operate(previousNumber, operator, currentNumber);
+    }
+    operator = button;
+    previousNumber = currentNumber;
+    currentNumber = "";
+    point = false;
+
+    if(answer) answer = false;
+    screen.textContent = previousNumber;
+    return previousNumber;
+}
+
+function pressEquals(button) {
+    let result = operate(previousNumber, operator, currentNumber);
+    currentNumber = String(result);
+    answer = true;
+    point = false;
+    operator = "";
+    return currentNumber;
+}
+
+function pressDelete(button) {
+    if (button == 'clear') {
+        currentNumber = "";
+        previousNumber = "";
+        operator = "";
+    }
+    else {
+        currentNumber = currentNumber.slice(0, -1);
+    }
+    return currentNumber;
+}
+
+//init
+
+const screen = document.querySelector("#screen");
+let buttons = document.querySelectorAll("td");
+
+let previousNumber = "";
+let currentNumber = "";
+let operator = "";
+let answer = false;
+let point = false;
+
+buttons.forEach((button) => {
+    if(button.id === "empty") return;
+    button.addEventListener('click', ()=>{
+        pressButton(button.id);
+    });
+});
